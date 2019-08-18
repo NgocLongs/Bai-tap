25e27912f7b90c5442c340022694e658a6afd619
Ngoc Longs <ngoclong1395@gmail.com>
2019-08-18 15:40:35 +0700

    edit php

--- chotot-javascript/upload.php
+++ chotot-javascript/upload.php
@@ -1,18 +1,18 @@
 <?php   
 
-    $countfiles = count($_FILES['file']['name']);
+    $countFiles = count($_FILES['file']['name']);
     $upload_location = "images/";
     $files_arr = array();
 
-    for($i = 0;$i < $countfiles;$i++){
+    for($i = 0;$i < $countFiles;$i++){
 
-        $filename = $_FILES['file']['name'][$i];
-        $ext = pathinfo($filename, PATHINFO_EXTENSION);
+        $fileName = $_FILES['file']['name'][$i];
+        $ext = pathinfo($fileName, PATHINFO_EXTENSION);
         $valid_ext = array("png","jpeg","jpg");
 
         if(($_FILES["file"]["size"][$i] <= 5000000) && in_array($ext, $valid_ext)) {
 
-            $path = $upload_location.$filename;
+            $path = $upload_location.$fileName;
 
             if(move_uploaded_file($_FILES['file']['tmp_name'][$i],$path)){
                 $files_arr[] = $path;
--- php/index.php
+++ php/index.php
@@ -1,29 +1,50 @@
 <?php 
-	$input = $index = '';
-	$error_input = $error_index = '';
-	if ($_SERVER["REQUEST_METHOD"] == "POST") {
-		if (!empty($_POST['submit'])) {
-			if (empty($_POST['input'])) {
-				$error_input = "<span style='color:red;'>Please Enter input</span>";
-			} else {
-				$input = $_POST['input'];
-			}
+	
+	$index = '';
+	$error_index = '';
 
-			if (empty($_POST['index'])) {
-				$error_index = "<span style='color:red;'>Please Enter index</span>";
-			} else {
-				$index = $_POST['index'];
-				if ($index > strlen($input)) {
-					$error_index = "<span style='color:red;'>Index must less than input length</span>";
-				}
-			}
+	
+	if (isset($_POST['submit']))
+	 {
+		
+		if (($_POST['index'] < 0)) {
+			$error_index = "<span style='color: red;'>Please enter the index greater than or equal to 0 </span>";
+		} else {
+			$index = $_POST['index'];
+		}		
+	}
 
-			if (!empty($_POST['input']) && !empty($_POST['index']) && ($index < strlen($input))) {
-				echo "<br>Index = ".$index."   Value = ".$input[$index];
-				echo "<br>Giá trị của chỉ số ".$index." là: ".substr($input, $index, 1);
-			}
+	
+	// 1 1 2 2 3 3 4 4 ... n  n
+
+	function GetValueOfIndex($index) {
+		if ($index == 0) {
+			return 1;
+		}
+
+		if ($index % 2 == 0) {
+			return GetValueOfIndex($index - 1) + 1;
+		} else {
+			return GetValueOfIndex($index - 1);
 		}
 	}
+
+	function GetValueOfIndex2($index) {
+		$before = 1;
+		$current = 1;
+
+		for ($i = 1; $i <= $index; $i++) {
+			if ($i % 2 == 0) {
+				$current = $before + 1;
+			} else {
+				$current = $before;
+			}
+			$before = $current;
+		}
+
+		return $current;
+	}	
+	
 ?>
 <!DOCTYPE html>
 <html lang="">
@@ -52,19 +73,21 @@
 	<body>
 		<h1 class="text-center">Hello World</h1>
 		<form class="form-group" action="index.php" method="POST">
-			<label for="">Nhập vào dãy số</label>
-			<input name="input" type="number" class="form-control" value='<?php echo isset($_POST['input']) ? $_POST['input'] : '' ?>'><?php echo $error_input; ?><br>
 
-			<label for="">Nhập tham số</label>
-			<input name="index" type="number" class="form-control" value='<?php echo isset($_POST['index'])? $_POST['index'] : '' ?>'><br><?php echo $error_index; ?><br>
+			<label for="">Enter Index: </label>
+			<input name="index" type="number" class="form-control" value='<?php echo isset($_POST['index'])? $_POST['index'] : '' ?>'><?php echo $error_index; ?><br>
 			<input name="submit" type="submit" class="btn btn-default">
 		</form>
-
+		<?php 
+			echo "Value At Index ".$index. " là " .GetValueOfIndex($index);
+			echo "Value At Index ".$index. " là " .GetValueOfIndex2($index); 
+		?>
+		
 		<!-- jQuery -->
 		<script src="//code.jquery.com/jquery.js"></script>
 		<!-- Bootstrap JavaScript -->
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
 		<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
- 		<script src="Hello World"></script>
+ 		<!-- <script src="Hello World"></script> -->
 	</body>
 </html>
